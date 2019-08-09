32b04ab72e33f6198554ef10cf8ba5478a01e55e
noahfranco <noahfra@gmail.com>
2019-08-08 21:56:56 -0400

    ran a test on App.js file and it was successful

--- testing-project/src/App.test.js
+++ testing-project/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("<App />", () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("render this page successfully", () => {
+    render(<App />)
+  })
+
+}); 
+
+
+
+
